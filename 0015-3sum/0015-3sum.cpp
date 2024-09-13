a9c3e93d6c98cb80005b63eec32c7a5ea19082fd
sruthy16 <66853359+sruthy16@users.noreply.github.com>
2024-09-13 16:12:01 -0700

    Time: 58 ms (77.23%), Space: 27.3 MB (91.18%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        //create result vector
         vector<vector<int>> result;
-        sort(nums.begin(), nums.end()); // Sort the array first
-
-        for (int i = 0; i < nums.size(); i++) {
-        // Skip duplicate elements for the first number
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1; // The left pointer
-            int right = nums.size() - 1; // The right pointer
-
-            while (left < right) {
+        
+        //sort the array
+        sort(nums.begin(), nums.end());
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            
+            int left = i+1;
+            int right = nums.size()-1;
+            
+            while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
+                if(sum == 0){
                     result.push_back({nums[i], nums[left], nums[right]});
-
-                    // Skip duplicates for the second number
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    // Skip duplicates for the third number
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
+                    while(left < right && nums[left] == nums[left+1]){
+                        left++;
+                    }
+                
+                    while(left < right && nums[right] == nums[right-1]){
+                        right--;
+                    }
                     left++;
                     right--;
-                } else if (sum < 0) {
-                    left++; // Move the left pointer to the right to increase the sum
+                } else if(sum < 0){
+                    left++;
                 } else {
-                    right--; // Move the right pointer to the left to decrease the sum
+                    right--;
                 }
             }
+            
         }
         return result;
     }
