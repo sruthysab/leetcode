d577e0a08b1e9b7db7510780ec1ddddb1a59d18c
sruthy16 <66853359+sruthy16@users.noreply.github.com>
2024-11-03 10:57:17 -0800

    Time: 457 ms (45.26%), Space: 11.8 MB (39.13%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n < 2) return 0;
+        // sieve of eratosthenes
+        vector<bool> isPrime(n+1, true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for(int i = 2; i <= sqrt(n); i++){
+            if(isPrime[i]){
+                for(int j = i*i; j <= n; j+=i){
+                    isPrime[j] = false;
+                }    
+            }
+        }
+        
+        int count = 0;
+        for(int i = 2; i < n; i++) {
+            if(isPrime[i]){
+                count++;
+            }
+        }
+    
+        return count;
+    }
+};
