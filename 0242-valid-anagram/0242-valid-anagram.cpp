af220992f470e9057ed9f4873d5e5e30c5b1144d
sruthy16 <66853359+sruthy16@users.noreply.github.com>
2024-11-08 10:32:57 -0800

    Time: 7 ms (37.80%), Space: 8.8 MB (55.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        
+        //handle Unicode characters
+        // For comprehensive Unicode and locale-aware sorting, the ICU (International //Components for Unicode) library is a good choice. ICU provides a Collator class that allows //locale-sensitive
+        
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
         return (s == t);
