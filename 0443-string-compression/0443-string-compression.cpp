a3b6e4008bc99339ee99a17e60ae928668a11d24
sruthy16 <66853359+sruthy16@users.noreply.github.com>
2024-11-09 16:47:15 -0800

    Time: 0 ms (100.00%), Space: 14 MB (34.33%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
+    // do we need to add 1 if count is is less than 1
     int compress(vector<char>& chars) {
-        int writeIndex = 0;
+        if(chars.size() == 1) {
+            return 1;
+        }
+      
+        int charIndex = 0; // don't need 2 indexes
         int count = 1;
-        
+        //for loop through chars
         for(int i = 1; i < chars.size(); i++){
             if(chars[i] == chars[i-1]){
-                count++;
+                count++; 
             } else {
-                chars[writeIndex] = chars[i-1];
-                writeIndex++;
+                chars[charIndex++] = chars[i-1];
                 if(count > 1){
-                    for(char c : to_string(count)){
-                        chars[writeIndex] = c;
-                        writeIndex++;
+                    string strC = to_string(count);
+                    for(char c: strC){
+                        chars[charIndex++] = c;
                     }
+                    count = 1;
                 }
-                count = 1;
+                // have to loop through to_string(count) to insert each number as a char
             }
         }
         
-        chars[writeIndex] = chars.back();
-        writeIndex++;
+        chars[charIndex++] = chars.back();
         if(count > 1){
-            for(char c : to_string(count)){
-                chars[writeIndex] = c;
-                writeIndex++;
+            string strC = to_string(count);
+            for(char c: strC){
+                chars[charIndex++] = c;
             }
         }
-        
-        return writeIndex;
-        
+    
+        chars.resize(charIndex);
+        return charIndex;
     }
 };
