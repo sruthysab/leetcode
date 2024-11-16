d1d24827416c7c72fdf2f5f86dd3c432a6a3f281
sruthy16 <66853359+sruthy16@users.noreply.github.com>
2024-11-16 13:59:28 -0800

    Time: 2 ms (24.29%), Space: 13 MB (21.21%) - LeetHub

---
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    bool checkCharRank(const string& a, const string& b, unordered_map<char, int> charRank){
+        int minLen = min(a.length(), b.length());
+        for(int i = 0; i < minLen; i++){
+            if(a[i] != b[i]){
+                return charRank[a[i]] < charRank[b[i]];
+            }
+        }
+        return a.length() <= b.length();
+    }
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        //what to return if vector is empty or has 1 string
+        if(words.size()<=1) return true;
+        
+        unordered_map<char, int> charRank;
+        for(int i = 0; i < order.length(); i++){
+            charRank[order[i]] = i;
+        }
+        
+        for(int i = 1; i < words.size(); i++){
+            if(!checkCharRank(words[i-1], words[i], charRank)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
