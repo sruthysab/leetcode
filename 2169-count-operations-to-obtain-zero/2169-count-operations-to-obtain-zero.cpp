23d1919062c5bba94840a6f987b1a3983e89e407
sruthy16 <66853359+sruthy16@users.noreply.github.com>
2024-11-20 10:15:05 -0800

    Time: 0 ms (100.00%), Space: 7.6 MB (5.93%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        long long op = 0;
+        while(num1 != 0 && num2 != 0){
+            if(num1 >= num2){
+                // num1 -= num2;
+                // op++; // reduce # of iterations
+                op += num1/num2;
+                num1 %= num2;
+            } else {
+                //num2 -= num1;
+                //op++;
+                op += num2/num1;
+                num2 %= num1;
+            }
+        }
+        return (op > INT_MAX) ? -1 : static_cast<int>(op);
+    }
+};
