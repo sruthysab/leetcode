420c42bd193deca7c87ea0240bd3d941d13d6cf7
sruthy16 <66853359+sruthy16@users.noreply.github.com>
2024-11-20 20:14:12 -0800

    Time: 23 ms (62.98%), Space: 37.3 MB (30.61%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> set1(nums1.begin(), nums1.end());
-        unordered_set<int> set2(nums2.begin(), nums2.end());
-
-        // Prepare vectors to store the results
-        vector<int> notIn2, notIn1;
-
-        // Find elements in nums1 that are not in nums2
-        for (int num : set1) {
-            if (set2.find(num) == set2.end()) {
-                notIn2.push_back(num);
+        unordered_set<int> s1(nums1.begin(), nums1.end());
+        unordered_set<int> s2(nums2.begin(), nums2.end());
+        
+        vector<int> diff1, diff2;
+        
+        for(const auto& num : s1){
+            if(s2.find(num) == s2.end()){
+                diff2.push_back(num);
             }
         }
-
-        // Find elements in nums2 that are not in nums1
-        for (int num : set2) {
-            if (set1.find(num) == set1.end()) {
-                notIn1.push_back(num);
+        
+        for(const auto& num : s2){
+            if(s1.find(num) == s1.end()){
+                diff1.push_back(num);
             }
-        }   
-
-        // Return the result as a vector of two vectors
-        return {notIn2, notIn1};
+        }
+        
+        return {diff2, diff1};
     }
 };
